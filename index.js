8133ce032942d23c89b7b4b67581befb42bbbf3c
Ridox_Studio <66634814+codad5@users.noreply.github.com>
2022-08-09 11:10:20 +0100

    currencynet

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express = require('express')
+const axios = require('axios')
+const app = express()
+const baseUrl = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}`
+
+app.get('/:from/:to/:price', async (req, res) => {
+    const {from, to} = req.params
+    console.log(from, to)
+    const price = req.params
+    axios.get(`${baseUrl}/pair/${from}/${to}`)
+         .then(response => {
+            console.log(response.data)
+            //send response to client
+            res.send({...response.data, status:true, from, to})
+         })
+         .catch(errors => {
+                console.log(errors)
+                // send a json respond with a status of 500 and the error message
+                res.status(500).send({error: errors.message, status:false})
+        })
+                
+        
+})
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`app listening at port ${process.env.PORT || 3000}`)
+})
